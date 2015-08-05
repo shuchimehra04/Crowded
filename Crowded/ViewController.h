176ec2556307b5e6f0d610ef2b97e3d3910c5d5b
shuchi <PoohBear@shuchis-MacBook-Pro.local>
2015-08-05 23:17:07 +0530

    Initial Commit

---
+++ Crowded/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Crowded
+//
+//  Created by shuchi mehra on 8/5/15.
+//  Copyright (c) 2015 shuchi mehra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Crowded/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Crowded
+//
+//  Created by shuchi mehra on 8/5/15.
+//  Copyright (c) 2015 shuchi mehra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
